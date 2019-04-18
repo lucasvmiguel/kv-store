7d48a5f298b150c665c1fa44f0a4ab6eaaf9a4fd
Lucas Vieira <lucas@Seans-MacBook-Pro-2.local>
2019-04-18 10:52:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# KV Store
+[![Build Status](https://travis-ci.org/lucasvmiguel/kv-store.svg?branch=master)](https://travis-ci.org/lucasvmiguel/kv-store)
+
+## Description
+
+## Installation
+
+```bash
+npm install --save kv-store
+```
+
+## How to use
+
+## Functions
+
+## RoadMap
+
+## License
+
+[MIT](LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,102 @@
+import * as mysql from 'mysql';
+
+type ClientTypes = 'mysql';
+
+interface IClient {
+    type: ClientTypes;
+    client: mysql.Connection;
+}
+
+class Client {
+    private type: ClientTypes;
+    private client: mysql.Connection;
+    private tableName: string = 'kvstore-keyvalues';
+
+    public async init(params: IClient): Promise<Boolean> {
+        this.client = params.client;
+        this.type = params.type;
+
+        switch (this.type) {
+        case 'mysql':
+            return this.initMysql();
+        }
+    }
+
+    public async get(key: string): Promise<string | null> {
+        switch (this.type) {
+        case 'mysql':
+            return this.getMysql(key);
+        }
+    }
+
+    public async put(key: string, value: string): Promise<Boolean> {
+        switch (this.type) {
+        case 'mysql':
+            return this.putMysql(key, value);
+        }
+    }
+
+    public async getJson(key: string): Promise<any | null> {
+        const result = await this.get(key);
+
+        if (result) {
+            return JSON.parse(result);
+        }
+
+        return null;
+    }
+
+    private async initMysql(): Promise<Boolean> {
+        const createTableQuery = `
+            CREATE TABLE IF NOT EXISTS \`${this.tableName}\` (
+                \`key\` VARCHAR(255) NOT NULL,
+                \`value\` TEXT NULL,
+                PRIMARY KEY (\`key\`)
+            );
+        `;
+
+        return new Promise<Boolean>((resolve, reject) => {
+            this.client.query(createTableQuery, (err) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(true);
+            });
+        });
+    }
+
+    private async getMysql(key: string): Promise<string | null> {
+        return new Promise<string | null>((resolve, reject) => {
+            const selectQuery = `SELECT \`${this.tableName}\`.value FROM \`${this.tableName}\` WHERE \`${this.tableName}\`.key = '${key}';`;
+            
+            this.client.query(selectQuery, (error, results) => {
+                if (error) {
+                    return reject(error);
+                }
+
+                if (Array.isArray(results) && results.length > 0) {
+                    return resolve(results[0].value);
+                }
+
+                return resolve(null);
+            });
+        });
+    }
+
+    private async putMysql(key: string, value: string): Promise<Boolean> {
+        return new Promise<Boolean>((resolve, reject) => {
+            const insertQuery = `INSERT INTO \`${this.tableName}\`(\`key\`, \`value\`) VALUES ('${key}','${value}') ON DUPLICATE KEY UPDATE \`${this.tableName}\`.value = '${value}';`;
+
+            this.client.query(insertQuery, (error) => {
+                if (error) {
+                    return reject(error);
+                }
+
+                return resolve(true);
+            });
+        });
+    }
+
+}
+
+export default new Client();
